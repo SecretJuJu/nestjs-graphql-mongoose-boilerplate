50438e7b7a4c47825346cc649e2bd36b1c056c55
SeungBo <47034129+SecretJuJu@users.noreply.github.com>
2021-09-06 17:54:20 +0900

    feat: app.module에 GQL 모듈 import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { PersonModule } from './person/person.module';
 import { HobbyModule } from './hobby/hobby.module';
 
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      'mongodb://localhost:27017/nestjs-graphql-mongoose-boilerplate',
-    ),
+    MongooseModule.forRoot('mongodb://localhost:27017/three-in-one-db'),
+    GraphQLModule.forRoot({
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+      playground: true,
+      debug: false,
+    }),
     PersonModule,
     HobbyModule,
   ],
--- src/app/person/person.inputs.ts
+++ src/app/person/person.inputs.ts
@@ -1,19 +1,37 @@
+import { Field, InputType } from '@nestjs/graphql';
 import { Schema as MongooseSchema } from 'mongoose';
+
 import { Hobby } from '../hobby/hobby.model';
 
+@InputType()
 export class CreatePersonInput {
+  @Field(() => String)
   name: string;
-  hobbies: Hobby[];
+
+  @Field(() => [String])
+  hobbies: MongooseSchema.Types.ObjectId[];
 }
 
+@InputType()
 export class ListPersonInput {
+  @Field(() => String, { nullable: true })
   _id?: MongooseSchema.Types.ObjectId;
+
+  @Field(() => String, { nullable: true })
   name?: string;
-  hobbies?: Hobby[];
+
+  @Field(() => [String], { nullable: true })
+  hobbies?: MongooseSchema.Types.ObjectId[];
 }
 
+@InputType()
 export class UpdatePersonInput {
+  @Field(() => String)
   _id: MongooseSchema.Types.ObjectId;
+
+  @Field(() => String, { nullable: true })
   name?: string;
-  hobbies?: Hobby[];
+
+  @Field(() => [String], { nullable: true })
+  hobbies?: MongooseSchema.Types.ObjectId[];
 }
