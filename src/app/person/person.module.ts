f5978ae8b28a405b25389dd4882128509afb436c
SeungBo <47034129+SecretJuJu@users.noreply.github.com>
2021-09-06 17:40:19 +0900

    chore: resolver 생성

--- src/app/person/person.module.ts
+++ src/app/person/person.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Person, PersonSchema } from './person.model';
 import { PersonService } from './person.service';
+import { PersonResolver } from './person.resolver';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Person.name, schema: PersonSchema }]),
   ],
-  providers: [PersonService],
+  providers: [PersonService, PersonResolver],
 })
 export class PersonModule {}
