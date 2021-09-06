572f7a6597c9b6f840f06c8a82242a14f7969fd6
SeungBo <47034129+SecretJuJu@users.noreply.github.com>
2021-09-06 16:59:44 +0900

    chore: person, hobby 모델정의

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PersonModule } from './person/person.module';
+import { HobbyModule } from './hobby/hobby.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb://localhost:27017/nestjs-graphql-mongoose-boilerplate',
+    ),
+    PersonModule,
+    HobbyModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
